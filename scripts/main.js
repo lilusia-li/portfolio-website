68a4b027bdf8bbcf0db0ab8b2239c7d6d8db764d
XXX <el.kresik@gmail.com>
2026-03-16 13:09:03 +0300

    (Projects) done project-tech-stack as slider

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,18 @@
-import { projectsLayout } from "./projectsLayout.js";
+import { projects, projectsLayout } from "./projectsLayout.js";
 
 const projectsContainer = document.querySelector("#projects-container");
 projectsContainer.insertAdjacentHTML("beforeend", projectsLayout);
+
+projects.forEach((project) => {
+  const swiper = new Swiper(`#${project.stackSwiperID}`, {
+    direction: "horizontal",
+    slidesPerView: "auto",
+    slidesPerGroup: 2,
+    spaceBetween: 10,
+
+    navigation: {
+      nextEl: `#${project.stackSwiperBtnNextID}`,
+      prevEl: `#${project.stackSwiperBtnPrevID}`,
+    },
+  });
+});
--- scripts/projectsLayout.js
+++ scripts/projectsLayout.js
@@ -1,8 +1,11 @@
-const projects = [
+export const projects = [
   {
     imageSrc: "./images/projects/future_tech.png",
     imageAlt: "Future-tech wallpaper",
     stack: ["HTML", "SCSS", "JS", "BEM", "Adaptive"],
+    stackSwiperID: "stack-swiper-Future-tech",
+    stackSwiperBtnPrevID: "stack-swiper-btn-prev-Future-tech",
+    stackSwiperBtnNextID: "stack-swiper-btn-next-Future-tech",
     name: "Future-tech",
     description: "Website layout.",
     openLink: "https://lilusia-li.github.io/future-tech",
@@ -11,7 +14,10 @@ const projects = [
   {
     imageSrc: "./images/projects/test_project_sepium.png",
     imageAlt: "Kitchen shop wallpaper",
-    stack: ["HTML", "SCSS", "JS", "Swiper", "Fancybox", "BEM", "Adaptive"],
+    stack: ["HTML", "CSS", "JS", "Swiper", "Fancybox", "BEM", "Adaptive"],
+    stackSwiperID: "stack-swiper-Kitchen-shop",
+    stackSwiperBtnPrevID: "stack-swiper-btn-prev-Kitchen-shop",
+    stackSwiperBtnNextID: "stack-swiper-btn-next-Kitchen-shop",
     name: "Kitchen shop",
     description: "Test project for 'Sepium'.",
     openLink: "https://lilusia-li.github.io/test-project-sepium",
@@ -21,11 +27,11 @@ const projects = [
 
 const createProjectCard = function (project) {
   const stackItems = project.stack
-    .map((tech) => `<li class="project-card__stack-item">${tech}</li>`)
+    .map((tech) => `<li class="swiper-slide stack__item">${tech}</li>`)
     .join("");
 
   return `
-    <div class="slider__card project-card">
+    <div class="project-card">
       <img
         class="project-card__image"
         src="${project.imageSrc}"
@@ -34,9 +40,21 @@ const createProjectCard = function (project) {
         height="201"
         loading="lazy"
       />
-      <ul class="project-card__stack-list">
-        ${stackItems}
-      </ul>
+      <div class="project-card__stack stack">
+        <div class="swiper stack__swiper" id=${project.stackSwiperID}>
+          <ul class="swiper-wrapper stack__list">
+            ${stackItems}
+          </ul>
+        </div>
+        <div
+          class="swiper-button-prev stack__button-prev"
+          id=${project.stackSwiperBtnPrevID}
+        ></div>
+        <div
+          class="swiper-button-next stack__button-next"
+          id=${project.stackSwiperBtnNextID}
+        ></div>
+      </div>
       <div class="project-card__specification">
         <h3 class="project-card__name">${project.name}</h3>
         <p class="project-card__description">${project.description}</p>
