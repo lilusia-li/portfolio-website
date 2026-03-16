70d2c3a0d5121269a8496fb2770dbdb662f81d80
XXX <el.kresik@gmail.com>
2026-03-16 21:19:34 +0300

    (Footer) done

--- scripts/main.js
+++ scripts/main.js
@@ -16,3 +16,8 @@ projects.forEach((project) => {
     },
   });
 });
+
+window.addEventListener("scroll", () => {
+  const header = document.querySelector("#header");
+  header.classList.toggle("scrolled", window.scrollY > 50);
+});
